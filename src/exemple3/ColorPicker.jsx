c3938292f01bca6ab112b375a92e70a932c744a3
Mathias Bernardeau <mathias.bernardeau@maif.fr>
2021-06-17 09:55:12 +0200

    Use Context.consumer in ColorPicker

--- src/exemple3/ColorPicker.jsx
+++ src/exemple3/ColorPicker.jsx
@@ -1,14 +1,15 @@
 import { Card } from "antd";
-import { useContext } from "react";
 import { HexColorPicker } from "react-colorful";
 import { ColorContext } from "./ColorContext";
 
 export function ColorPicker() {
-  const { color, setColor } = useContext(ColorContext);
-
   return (
     <Card title="Choisir une couleur" className="example-card">
-      <HexColorPicker color={color} onChange={setColor} />
+      <ColorContext.Consumer>
+        {({ color, setColor }) => (
+          <HexColorPicker color={color} onChange={setColor} />
+        )}
+      </ColorContext.Consumer>
     </Card>
   );
 }
